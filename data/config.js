c80f98543735ee6ee9defdbda82f264d09090e73
ntapanlis <ntapanlis@gmail.com>
2026-09-05 03:13:26 +0200

    Add files via upload

--- data/config.js
+++ data/config.js
@@ -1,4 +1,4 @@
 // Paste your published Google Sheet's CSV link here (see README.md for
 // exactly how to get it). Leave as null to use the books already listed in
 // data/books.js instead - handy for offline editing/testing.
-window.SHEET_CSV_URL = null;
+window.SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQxVOA0YavYj-hDSCpiUB0xP4Dp7uJqwP7-j93OAhmVWLWXlIZFYF1m0KU1NF--bd98LFGEp2eB8HV_/pub?output=csv";
